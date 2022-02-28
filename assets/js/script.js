41472bd187117361b1ec30e708edfdbe787b8487
Vicente Garcia <vicen1@hotmail.com>
2022-02-28 15:45:49 -0600

    Add tasks edition

--- assets/js/script.js
+++ assets/js/script.js
@@ -33,7 +33,8 @@ var loadTasks = function() {
 
   // loop over object properties
   $.each(tasks, function(list, arr) {
-    console.log(list, arr);
+    //console.log(list, arr);
+    console.log($);
     // then loop over sub-array
     arr.forEach(function(task) {
       createTask(task.text, task.date, list);
@@ -45,8 +46,91 @@ var saveTasks = function() {
   localStorage.setItem("tasks", JSON.stringify(tasks));
 };
 
+// Using JQuery to update textarea task
+$(".list-group").on("click", "p", function(){
+  // Get current text
+  var text = $(this)
+  .text()
+  .trim();
+  // Create new imput emelement
+  var textImput = $("<textarea>")
+  .addClass("form-control")
+  .val(text);
+  // Swap out elements
+  $(this).replaceWith(textImput);
+  // Focus on new element
+  textImput.trigger("focus");
+  console.log(text);
+});
 
+// Function when blur update textarea
+$(".list-group").on("blur", "textarea", function(){
+  // Get the textarea's current value/text
+  var text = $(this)
+  .val()
+  .trim();
+  // Get the parent ul's id attribute
+  var status = $(this)
+  .closest(".list-group")
+  .attr("id")
+  .replace("list-", "");
+  // Get the task's position in the list of other li elements
+  var index = $(this)
+  .closest(".list-group-item")
+  .index();
+  // Replace value in array
+  tasks[status][index].text = text;
+  saveTasks();
+  // Recrate p element
+  var taskP = $("<p>")
+  .addClass("m-1")
+  .text(text);
+  // Replace textarea with p element
+  $(this).replaceWith(taskP);
+});
 
+// Update date task
+$(".list-group").on("click", "span", function(){
+  // Get current date (text)
+  var date = $(this)
+  .text()
+  .trim();
+  // Create new input element
+  var dateImput = $("<input>")
+  .attr("type", "text")
+  .addClass("form-control")
+  .val(date);
+  // Swap out elements
+  $(this).replaceWith(dateImput);
+  // Focus on new element
+  dateImput.trigger("focus");
+});
+
+// Function when blur update date
+$(".list-group").on("blur", "input[type='text']", function(){
+  // Get the current date value/text
+  var date = $(this)
+  .val()
+  .trim();
+  // Get the parent ul's id attribute
+  var status = $(this)
+  .closest(".list-group")
+  .attr("id")
+  .replace("list-", "");
+  // Get the task's position in the list of other li elements
+  var index = $(this)
+  .closest(".list-group-item")
+  .index();
+  // Replace value in array
+  tasks[status][index].date = date;
+  saveTasks();
+  // Recrate span element
+  var taskSpan = $("<span>")
+  .addClass("badge badge-primary badge-pill")
+  .text(date);
+  // Replace input with span element
+  $(this).replaceWith(taskSpan);
+});
 
 // modal was triggered
 $("#task-form-modal").on("show.bs.modal", function() {
@@ -93,5 +177,3 @@ $("#remove-tasks").on("click", function() {
 
 // load tasks for the first time
 loadTasks();
-
-

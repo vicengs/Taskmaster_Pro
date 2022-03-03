f57ce0c48af9c28baa3d10a8fa434a0e59f0107f
Vicente Garcia <vicen1@hotmail.com>
2022-03-03 12:28:52 -0600

    UX enhancements

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,7 @@
 /* File     : script.js      */
 /* Author   : Vicente Garcia */
 /* Date     : 02/28/2022     */
-/* Modified : 02/28/2022     */
+/* Modified : 03/03/2022     */
 /* ------------------------- */
 var tasks = {};
 
@@ -164,16 +164,18 @@ $(".card .list-group").sortable({
   tolerance: "pointer",
   helper: "clone",
   activate: function(event) {
-    console.log("activate", this);
+    $(this).addClass("dropover");
+    $(".bottom-trash").addClass("bottom-trash-drag");
   },
   deactivate: function(event) {
-    console.log("deactivate", this);
+    $(this).removeClass("dropover");
+    $(".bottom-trash").removeClass("bottom-trash-drag");
   },
   over: function(event) {
-    console.log("over", event.target);
+    $(event.target).addClass("dropover-active");
   },
   out: function(event) {
-    console.log("out", event.target);
+    $(event.target).removeClass("dropover-active");
   },
   update: function(event) {
     // Array to store the task data in
@@ -209,12 +211,13 @@ $("#trash").droppable({
   tolerance: "touch",
   drop: function(event, ui){
     ui.draggable.remove();
+    $(".bottom-trash").removeClass("bottom-trash-active");
   },
   over: function(event, ui){
-    console.log("over");
+    $(".bottom-trash").addClass("bottom-trash-active");
   },
   out: function(event, ui){
-    console.log("out");
+    $(".bottom-trash").removeClass("bottom-trash-active");
   }
 });
 
@@ -235,7 +238,7 @@ $("#task-form-modal").on("shown.bs.modal", function() {
 });
 
 // save button in modal was clicked
-$("#task-form-modal .btn-primary").click(function() {
+$("#task-form-modal .btn-save").click(function() {
   // get form values
   var taskText = $("#modalTaskDescription").val();
   var taskDate = $("#modalDueDate").val();
